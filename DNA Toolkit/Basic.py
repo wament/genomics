217358eb7421fed6ca5d21c12e40ed0f365da20d
William Ament <will@ament.net>
2023-10-19 15:27:30 -0500

    Update to Basic.py

--- DNA Toolkit/Basic.py
+++ DNA Toolkit/Basic.py
@@ -11,19 +11,32 @@ import collections
 # Base Pairs for DNA    A <-> T  |  C <-> G
 Nucleotides = ["A", "C", "G", "T"]
 
+NucComplement = {'A':'T', 'T':'A', 'C':'G', 'G':'C'}
+
 #dna_seq is a dirty input, 
 # we want to validate that it's a real DNA seq
 def validateSeq(dna_seq):
+    """Checks for invalid characters in the DNA sequence.
+
+    Args:
+        dna_seq (string): A string of characters A,C,G,T 
+
+    Returns:
+        _type_: _description_
+    """
     seq = dna_seq.upper()
     for nuc in seq:
         if nuc not in Nucleotides:
             print("\nThe sequence contains an invalid char{} ",nuc)
             return False
+    print("Valid Sequence")
     return seq
 
 def countNuc(seq):
-    counts = {"A": 0, "C": 0, "G": 0, "T": 0}
-    for nuc in seq:
-        counts[nuc]+=1
-    return counts
-    #return dict(collections.Counter(seq))
+    return dict(collections.Counter(seq))
+
+def invertDNA(seq):
+    return seq.reverse()
+
+def DNAtranscription(seq):
+    return seq.replace("T","U")
